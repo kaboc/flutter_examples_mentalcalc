0635b86cba52b9099b848588be2493b1297ad663
Kabocha <20254485+kaboc@users.noreply.github.com>
2019-03-28 02:21:07 +0900

    Tiny stylistic change

--- bloc/lib/blocs/calc_provider.dart
+++ bloc/lib/blocs/calc_provider.dart
@@ -3,9 +3,9 @@ import 'calc_bloc.dart';
 export 'calc_bloc.dart';
 
 class CalcBlocProvider extends InheritedWidget {
-  CalcBloc get bloc => CalcBloc();
-
   const CalcBlocProvider({Key key, Widget child}) : super(key: key, child: child);
+
+  CalcBloc get bloc => CalcBloc();
 
   @override
   bool updateShouldNotify(oldWidget) => oldWidget != this;
