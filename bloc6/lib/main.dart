799ebba67d3a11fab76513db441dbe689f6acbc3
Kabocha <20254485+kaboc@users.noreply.github.com>
2020-08-05 09:03:39 +0900

    Set lazy to false to instantiate RecordCubit soon
    to fix the issue where it did not start listening to CalcState, which prevented RecordState from being updated at the end of the first challenge.

--- bloc6/lib/main.dart
+++ bloc6/lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
         BlocProvider<RecordCubit>(
+          lazy: false,
           create: (context) => RecordCubit(
             calcCubit: context.bloc<CalcCubit>(),
           ),
