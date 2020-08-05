e955fb1e16021d18dc06a379f2f4f42633545d8f
Kabocha <20254485+kaboc@users.noreply.github.com>
2020-08-05 09:09:51 +0900

    Remove sum from RecordState and add getter instead

--- bloc5/lib/model/record_state.dart
+++ bloc5/lib/model/record_state.dart
@@ -3,30 +3,22 @@ import 'package:equatable/equatable.dart';
 
 @immutable
 class RecordState extends Equatable {
-  const RecordState({
-    @required this.times,
-    @required this.numbers,
-    @required this.sum,
-  });
+  const RecordState({@required this.times, @required this.numbers});
 
   final int times;
   final List<int> numbers;
-  final int sum;
 
-  static const none = RecordState(times: 0, numbers: [], sum: 0);
+  int get sum => numbers.fold<int>(0, (a, b) => a + b);
+
+  static const none = RecordState(times: 0, numbers: []);
 
   @override
-  List<Object> get props => [times, numbers, sum];
+  List<Object> get props => [times, numbers];
 
   RecordState copyWith({List<int> numbers}) {
     return RecordState(
       times: times + 1,
       numbers: numbers ?? this.numbers,
-      sum: _sum(numbers),
     );
-  }
-
-  int _sum(List<int> numbers) {
-    return numbers.fold<int>(0, (a, b) => a + b);
   }
 }
